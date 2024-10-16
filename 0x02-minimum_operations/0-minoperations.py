cde0b821060afc370c4573174e836ad72a59e2f1
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-16 16:18:06 +0000

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Minimum operations
+"""
+
+
+def minOperations(n):
+    """
+    Minimum operations
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
+
+
+if __name__ == "__main__":
+    print(minOperations(4))
+    print(minOperations(12))
+    print(minOperations(9))
