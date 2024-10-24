4f4ee889763e312d0985f44ca5b59e2c78bb6542
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-24 19:35:31 +0000

    tes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -21,28 +21,31 @@ if __name__ == "__main__":
     """
     i = 0
     filesSize = 0
-    dec = {'200': 0,
-                   '301': 0,
-                   '400': 0,
-                   '401': 0,
-                   '403': 0,
-                   '404': 0,
-                   '405': 0,
-                   '500': 0}
-
+    dec = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
     try:
         for line in sys.stdin:
-            args = line.split(' ')
-            if len(args) > 2:
-                status_line = args[-2]
-                file_size = args[-1]
-                if status_line in dec:
-                    dec[status_line] += 1
-                filesSize += int(file_size)
-                i += 1
-                if i == 10:
-                    print_stats(filesSize, dec)
-                    i = 0
-
+            text = line.split()
+            if len(text) > 2:
+                if text[-2] in dec:
+                    dec[text[-2]] += 1
+            else:
+                continue
+            filesSize += int(text[-1])
+            i += 1
+            if i == 10:
+                i = 0
+                print_stats(filesSize, dec)
+    except Exception:
+        pass
     finally:
         print_stats(filesSize, dec)
+        raise
