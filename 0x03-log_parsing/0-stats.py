a0077d6da42f5ffd1f0439c0fce20912d349328c
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-24 19:33:31 +0000

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -21,27 +21,42 @@ if __name__ == "__main__":
     """
     i = 0
     filesSize = 0
-    dec = {'200': 0,
-                   '301': 0,
-                   '400': 0,
-                   '401': 0,
-                   '403': 0,
-                   '404': 0,
-                   '405': 0,
-                   '500': 0}
+    dec = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
     try:
         for line in sys.stdin:
-            args = line.split(' ')
-            if len(args) > 2:
-                status_line = args[-2]
-                file_size = args[-1]
-                if status_line in dec:
-                    dec[status_line] += 1
-                filesSize += int(file_size)
-                i += 1
-                if i == 10:
-                    print_stats(filesSize, dec)
-                    i = 0
+            text = line.split()
+            filesSize += int(text[-1])
+            if text[-2] == "200":
+                dec["200"] += 1
+            if text[-2] == "301":
+                dec["301"] += 1
+            if text[-2] == "400":
+                dec["400"] += 1
+            if text[-2] == "401":
+                dec["401"] += 1
+            if text[-2] == "403":
+                dec["403"] += 1
+            if text[-2] == "404":
+                dec["404"] += 1
+            if text[-2] == "405":
+                dec["405"] += 1
+            if text[-2] == "500":
+                dec["500"] += 1
+            i += 1
+            if i == 10:
+                i = 0
+                print_stats(filesSize, dec)
+    except Exception:
+        pass
     finally:
         print_stats(filesSize, dec)
         raise
