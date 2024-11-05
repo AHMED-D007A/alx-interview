c90c8aae2b2435ea080680526642505c3645b08e
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-11-05 17:39:47 +0000

    done

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+N queens
+The N queens puzzle is the challenge of placing N non-attacking
+queens on an N*N chessboard.
+"""
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    def isSafe(board, row, col):
+        for i in range(col):
+            if board[i] == row or board[i] == row - col + i or board[i] == row + col - i:
+                return False
+        return True
+
+    def solveNQueens(board, col):
+        if col == N:
+            print(str([[i, board[i]] for i in range(N)]))
+            return
+        for i in range(N):
+            if isSafe(board, i, col):
+                board[col] = i
+                solveNQueens(board, col + 1)
+    solveNQueens([0 for i in range(N)], 0)
