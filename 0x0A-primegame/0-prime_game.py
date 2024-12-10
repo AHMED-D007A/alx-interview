475bdffb3ce366c7fc5ab7d152d5c87627b16930
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-12-10 05:59:57 +0000

    test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -18,21 +18,18 @@ def is_prime(n):
 
 def isWinner(x, nums):
     """Prime Game"""
-    if x < 1 or nums is None:
+    if not nums or x < 1:
         return None
-
     n = max(nums)
     primes = [0] * (n + 1)
     for i in range(1, n + 1):
         primes[i] = primes[i - 1]
         if is_prime(i):
             primes[i] += 1
-
     winner = 0
     for n in nums:
-        winner ^= primes[n] % 2
-
-    return "Maria" if winner else "Ben"
+        winner ^= (primes[n] % 2 == 0)
+    return "Ben" if winner else "Maria"
 
 
 if __name__ == "__main__":
